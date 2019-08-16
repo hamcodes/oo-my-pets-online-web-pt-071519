2da716205762fdd73a53f20e7d98bacfcbf9c054
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-16 06:32:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,62 @@
 class Owner
-  # code goes here
+  attr_accessor :cats, :dogs
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @cats = []
+    @dogs = []
+    @@all << self
+  end
+  def name
+    @name
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+   
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    @cats
+  end
+  
+  def dogs
+    @dogs
+  end
+    
+  def buy_cat(cat)
+    Cat.new(cat, self) 
+  end
+  
+    def buy_dog(dog)
+    Dog.new(dog, self) 
+  end
+  
+  def walk_dogs
+    @dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    @cats.each {|cat| cat.mood = "happy"
+    }
+  end
+  
+  def sell_pets
+    
+    
+    
+  
 end
