df560b35434b62b7acc91c806b88e80b82763895
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-16 06:56:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,6 +66,6 @@ end
 #        expect(@owner.list_pets).to eq("I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s).")
   def list_pets
     "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
-    
+  end
   
 end
